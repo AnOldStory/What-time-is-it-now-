377f66eb93c5123b1d32657590163a751e616395
Hyeong Chang Lee <hc9904@naver.com>
2018-12-09 16:45:24 +0900

    Updates

---
+++ precache-manifest.b971e6359e85dd979f83d37958ed36cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "be7ca5d4d2bcbe6f64de",
+    "url": "/WhatTimeIsItNow/static/js/runtime~main.be7ca5d4.js"
+  },
+  {
+    "revision": "74b963d7c6627f3838b9",
+    "url": "/WhatTimeIsItNow/static/js/main.74b963d7.chunk.js"
+  },
+  {
+    "revision": "7f2cc64931ebcd666d52",
+    "url": "/WhatTimeIsItNow/static/js/1.7f2cc649.chunk.js"
+  },
+  {
+    "revision": "74b963d7c6627f3838b9",
+    "url": "/WhatTimeIsItNow/static/css/main.d6615309.chunk.css"
+  },
+  {
+    "revision": "ae4f7d0cfb8ae3f25233df3a41924d3e",
+    "url": "/WhatTimeIsItNow/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/WhatTimeIsItNow/precache-manifest.71ec2a532bfc220b8f9cb4a37cea18f1.js"
+  "/WhatTimeIsItNow/precache-manifest.b971e6359e85dd979f83d37958ed36cd.js"
 );
 
 workbox.clientsClaim();
